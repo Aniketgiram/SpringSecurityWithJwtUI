42b9507d035a2468147780c3d668ebed391360cc
giram-a <giram.a@northeastern.edu>
2024-10-26 14:33:39 -0400

    User profile added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const [jwt, setJwt] = useState("");
+  const [profile, setProfile] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -32,6 +33,7 @@ function App() {
         console.log(data);
         setJwt(data.jwtToken);
         setMessage("Login Successful");
+        fetchProfile(data.jwtToken);
       } else {
         console.log(response);
         setMessage("Login Failed")
@@ -42,23 +44,65 @@ function App() {
     }
   }
 
+  const fetchProfile = async (token) => {
+    try {
+      const myHeaders = new Headers();
+
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+      };
+
+      console.log(token);
+
+
+      const response = await fetch("http://localhost:8080/profile", requestOptions)
+
+      console.log(response);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setProfile(data)
+      } else {
+        console.log(response);
+        setMessage("Login to fetch profile")
+      }
+    } catch (e) {
+      console.log(e);
+      setMessage("Some Error Occured while fetching profile");
+    }
+  }
+
   return (
     <>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
-      </form>
 
-      {message && <p>{message}</p>}
-      {jwt && <p>{jwt}</p>}
+      {!profile ? (
+        <>
+          <h1>Login</h1>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+          {message && <p>{message}</p>}
+        </>
+
+      ) : (
+        <div>
+          <h3>User Profile</h3>
+          <p>Username: {profile.username}</p>
+          <p>Roles: {profile.role.join(",")}</p>
+        </div>
+      )}
     </>
   )
 }
